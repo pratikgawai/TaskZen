e5c43be8d9c3eef8e48a6ddee34d2a43e71ef70d
Pratik Gawai <pratikgawai786@gmail.com>
2026-07-18 17:20:47 +0530

    frontend ready for deploy

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://taskzen-backend-ae5d.onrender.com",
+  baseURL: "https://taskzen-backend-ae5d.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
