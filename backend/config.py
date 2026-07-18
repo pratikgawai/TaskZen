ab8159aacd3355bcba190e0ea4a589a1ad98c7fa
Pratik Gawai <pratikgawai786@gmail.com>
2026-07-18 13:21:17 +0530

    Fix config

--- backend/config.py
+++ backend/config.py
@@ -25,9 +25,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY")
+    SECRET_KEY = os.getenv("taskzen_secret_key")
 
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+    JWT_SECRET_KEY = os.getenv("taskzen_backend_project_2026_super_secret_key_123456789")
 
     SQLALCHEMY_DATABASE_URI = (
         f"mysql+pymysql://{os.getenv('root')}:"
