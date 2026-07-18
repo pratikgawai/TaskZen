4c81398e0b8020d9da05e740b45402a1e96ee7aa
Pratik Gawai <pratikgawai786@gmail.com>
2026-07-18 12:49:59 +0530

    Fix SQLAlchemy config

--- backend/config.py
+++ backend/config.py
@@ -1,3 +1,24 @@
+# import os
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# class Config:
+#     SECRET_KEY = os.getenv("SECRET_KEY")
+
+#     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+
+# SQLALCHEMY_DATABASE_URI = (
+
+#     f"mysql+pymysql://{os.getenv('root')}:"
+#     f"{os.getenv('mgRlIkkGLVYLzVWhNjJmvsXMGwIzsXDo')}@"
+#     f"{os.getenv('mysql.railway.internal')}:"
+#     f"{os.getenv('3306')}/"
+#     f"{os.getenv('railway')}"
+# )
+# SQLALCHEMY_TRACK_MODIFICATIONS = False              
+
+
 import os
 from dotenv import load_dotenv
 
@@ -8,12 +29,12 @@ class Config:
 
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 
-SQLALCHEMY_DATABASE_URI = (
+    SQLALCHEMY_DATABASE_URI = (
+        f"mysql+pymysql://{os.getenv('root')}:"
+        f"{os.getenv('mgRlIkkGLVYLzVWhNjJmvsXMGwIzsXDo')}@"
+        f"{os.getenv('mysql.railway.internal')}:"
+        f"{os.getenv('3306')}/"
+        f"{os.getenv('railway')}"
+    )
 
-    f"mysql+pymysql://{os.getenv('root')}:"
-    f"{os.getenv('mgRlIkkGLVYLzVWhNjJmvsXMGwIzsXDo')}@"
-    f"{os.getenv('mysql.railway.internal')}:"
-    f"{os.getenv('3306')}/"
-    f"{os.getenv('railway')}"
-)
-SQLALCHEMY_TRACK_MODIFICATIONS = False              
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
